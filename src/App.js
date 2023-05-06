828da1be1c0c79153125d3baeac765edf3b858db
10xvick <103027108+10xvick@users.noreply.github.com>
2023-05-06 22:50:51 +0530

    1 file modified, 22 added, 19 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import React from "react";
-import "./style.css";
+import logo from './logo.svg';
+import React,{useState} from 'react';
+import './App.css';
+import { Box , Container, Typography } from '@mui/material';
+import Header2 from './Components/Header2'
+import Header from './Components/Header.jsx'
+import AppCode from './Components/AppCode';
+import { Card } from '@mui/material';
+import { Paper } from '@mui/material';
+import ButtonCom from './Components/ButtonCom';
+import GridApp from './Components/GridApp';
+import {ContainerStyle} from "./CommonStyle"
 
-export default function App() {
+
+
+function App() {
+
+  const [message , setMessage] = useState('');
+  const [message1 , setMessage1] = useState('');
+  const [message2 , setMessage2] = useState('');
+  const [message3 , setMessage3] = useState('');
+  const [message4 , setMessage4] = useState('');
+  const [message5 , setMessage5] = useState('')
+  
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen )</p>
-    </div>
+    <div className="App">
+     <Box sx={ContainerStyle}>
+      <Header/>
+    <Header2  setMessage={setMessage} setMessage1={setMessage1} setMessage2={setMessage2} setMessage3={setMessage3}
+    setMessage4={setMessage4}  setMessage5={setMessage5}
+    />
+      <AppCode  message={message} message1={message1} message2={message2} message3={message3} message4={message4}  
+     message5={message5}
+    />
+{/* <GridApp message={message} message1={message1} message2={message2} message3={message3} message4={message4}  
+     message5={message5}/> */}
+
+    {/* <Card sx={{ borderBottom:"6px solid back" , backgroundColor:"blue" , mb:"2px" , color:"white" , height:"59"}} square elevation={0}>
+    <Typography sx={{float:"left" , ml:"70px" , fontSize:"19px" }} p={1} > <b>Interest Parameters</b></Typography>
+    </ Card> */}
+
+    {/* <AppCode  message={message} message1={message1} message2={message2} message3={message3} message4={message4}  
+     message5={message5}
+    /> */}
+    {/* <Card sx={{ borderBottom:"6px solid back" , backgroundColor:"blue" ,  mb:"0px" , position:"absolute", color:"white" , height:"59"}} square elevation={0}>
+    <Typography sx={{float:"left" , ml:"80px" , fontSize:"19px" }} p={1} > <b>Proceed</b></Typography>
+    </ Card> */}
+    </Box >
+</div>
   );
 }
+
+export default App;
